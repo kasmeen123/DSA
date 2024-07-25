af0bfef913cb5758275f75ff2761deaa91d01eb6
Kashish MEENA <102368215+kasmeen123@users.noreply.github.com>
2024-07-25 09:12:03 +0530

    Time: 5 ms (96.34%), Space: 41.4 MB (70.31%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,28 +1,58 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n = s.length();
-        int cnt = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j <= n; j++){
-                String subString = s.substring(i, j);
-                if(palindrome(subString)){
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
+//         int n = s.length();
+//         int cnt = 0;
+//         for(int i = 0; i < n; i++){
+//             for(int j = i+1; j <= n; j++){
+//                 String subString = s.substring(i, j);
+//                 if(palindrome(subString)){
+//                     cnt++;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
     
-    public boolean palindrome(String string) {
-        int left = 0;
-        int right = string.length() - 1;
-        while(left <= right){
-            if(string.charAt(left) != string.charAt(right)){
-                return false;
+//     public boolean palindrome(String string) {
+//         int left = 0;
+//         int right = string.length() - 1;
+//         while(left <= right){
+//             if(string.charAt(left) != string.charAt(right)){
+//                 return false;
+//             }
+//             left++;
+//             right--;
+//         }
+//         return true;
+        
+         int cnt = 0;
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+        // FOR ODD LENGTH PALINDROME
+        int l = i;
+        int r = i;
+        while (l >= 0 && r < n) {
+            if (s.charAt(l) == s.charAt(r)) {
+                cnt++;
+                l--;
+                r++;
+            } else {
+                break; // Added break to exit the loop when characters don't match
             }
-            left++;
-            right--;
         }
-        return true;
+        // FOR EVEN LENGTH PALINDROME
+        l = i;
+        r = i + 1;
+        while (l >= 0 && r < n) {
+            if (s.charAt(l) == s.charAt(r)) {
+                cnt++;
+                l--;
+                r++;
+            } else {
+                break; // Added break to exit the loop when characters don't match
+            }
+        }
+    }
+    return cnt;
     }
 }
