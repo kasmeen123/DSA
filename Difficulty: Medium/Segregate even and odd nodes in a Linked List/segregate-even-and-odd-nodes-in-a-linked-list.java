b92d23c166bbcf7cf023f7989f2afe8b6daf5d0f
Kashish MEENA <102368215+kasmeen123@users.noreply.github.com>
2024-09-13 14:37:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Segregate even and odd nodes in a Linked List/segregate-even-and-odd-nodes-in-a-linked-list.java
@@ -0,0 +1,129 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    Integer nextInt() { return Integer.parseInt(next()); }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG {
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t > 0) {
+
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            Node ans = ob.divide(head);
+            printList(ans);
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node divide(Node head) {
+        // code here
+        Node even = null;
+        Node odd = null;
+        Node e = null;
+        Node o = null;
+        
+        while (head != null) {
+            if(head.data % 2 == 0) {
+                if(even == null) {
+                    even = head;
+                    e = head;
+                }
+                else {
+                    e.next = head;
+                    e = e.next;
+                }
+            }
+            else {
+                 if(odd == null) {
+                    odd = head;
+                    o = head;
+                }
+                else {
+                    o.next = head;
+                    o = o.next;
+                }
+            }
+            head = head.next;
+        }
+        if (o != null) o.next = null;
+        if (even == null) return odd;
+        if(e != null) e.next = odd;
+        return even;
+    }
+}
