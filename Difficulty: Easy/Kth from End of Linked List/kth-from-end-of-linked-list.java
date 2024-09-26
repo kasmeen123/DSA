3b47b8a06775e27aa8b9c956e5854c7bd71ab14a
Kashish MEENA <102368215+kasmeen123@users.noreply.github.com>
2024-09-26 18:46:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Kth from End of Linked List/kth-from-end-of-linked-list.java
+++ Difficulty: Easy/Kth from End of Linked List/kth-from-end-of-linked-list.java
@@ -31,34 +31,69 @@ class Solution {
     int getKthFromLast(Node head, int k) {
         // Your code here
         
-        Node prev = null;
-        Node curr = head;
+        // Node prev = null;
+        // Node curr = head;
 
         
-          while(curr != null) {
-            Node next = curr.next;
+        //   while(curr != null) {
+        //     Node next = curr.next;
             
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
+        //     curr.next = prev;
+        //     prev = curr;
+        //     curr = next;
+        // }
             
-            head = prev;
+        //     head = prev;
         
-         int cnt = 1;
+        //  int cnt = 1;
          
-         Node node = head;
+        //  Node node = head;
          
-         while (node != null) {
-              if(cnt == k) {
-                  return node.data;
-              }
-              node = node.next;
-              cnt++;
-         }
+        //  while (node != null) {
+        //       if(cnt == k) {
+        //           return node.data;
+        //       }
+        //       node = node.next;
+        //       cnt++;
+        //  }
          
          
-         return -1;
+        //  return -1;
+        
+        // int cnt = 0;
+        // Node curr = head;
+        
+        // while (curr != null) {
+        //     curr = curr.next;
+        //     cnt++;
+        // }
+        
+         
+        // curr = head;
+        // int i = 0;
+        // while (curr != null){
+        //     if(i == cnt-k) return curr.data;
+        //     curr = curr.next;
+        //     i++;
+        // }
+        // return -1;
+        
+        
+        Node fast = head;
+        int i = 0;
+        while (i < k) {
+            if (fast == null) {
+                return -1; // If k is greater than the length of the list
+            }
+            fast = fast.next;
+            i++;
+        }
+        Node slow = head;
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow.data;
     }
 }
 
